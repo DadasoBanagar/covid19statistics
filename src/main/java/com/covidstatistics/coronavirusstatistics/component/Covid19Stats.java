e38dce2348066a871e3e41fb0e9b9211879c2526
DadasoBanagar <91798016+DadasoBanagar@users.noreply.github.com>
2022-06-13 11:39:43 +0530

    Added GeneratedValue annotation to generate id automatically

--- src/main/java/com/covidstatistics/coronavirusstatistics/component/Covid19Stats.java
+++ src/main/java/com/covidstatistics/coronavirusstatistics/component/Covid19Stats.java
@@ -1,6 +1,8 @@
 package com.covidstatistics.coronavirusstatistics.component;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class Covid19Stats {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String city;
 	private String province;
